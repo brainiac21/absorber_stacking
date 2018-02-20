8f5308c657b325e56106218c6ec7abfd65209b3a
Bill Zhu <duduxiong10@gmail.com>
2018-02-19 16:54:44 -0800

    Update psf_subtraction_sdss.py
    Updated the flux-limited masks.

--- psf_subtraction_sdss.py
+++ psf_subtraction_sdss.py
@@ -26,6 +26,19 @@ field = Q_data['FIELD_NUMBER']
 
 image_path = '/image_quasar/DR12/'
 
+# Load ezgal model file with 100 Myr burst, low metallicity
+model = ezgal.model('www.baryons.org/ezgal/models/cb07_burst_0.1_z_0.008_chab.model')
+#model = ezgal.model('www.baryons.org/ezgal/models/bc03_ssp_z_0.02_chab.model')
+
+# Desired formation redshift
+zf = 2.0
+# Fetch an array of redshifts out to given formation redshift
+zs = np.arange(0.005, zf, 0.005)
+#print(np.shape(zs))
+model.set_normalization('sloan_g', 0.00000000000000001, -22.4)
+thresh_mags = model.get_apparent_mags(zf, filters=['sloan_u', 'sloan_g', 'sloan_r', 'sloan_i', 'sloan_z'], zs=zs)
+
+
 for i in np.arange(run.size):
     file_name  = 'frame-%06d-g%d-%04d-%d.fit' % ( int(run[i]), int(col[i]), int(filed[i]), int(rerun[i]))
     image_frame = fits.open(image_path+file_name)
@@ -37,8 +50,6 @@ for i in np.arange(run.size):
 
     quasar = obj_table[q_id - 1]
 
-
-
 	quasar_xc = quasar['COLC'][pointer]
 	quasar_yc = quasar['ROWC'][pointer]
 
@@ -46,7 +57,40 @@ for i in np.arange(run.size):
 
 # here: check the quasar zoom-in image quality and perform the masking algorithm.
 
-    final_image =  mask_algorithms(image, quasar_xc, quasar_yc, obj_table, chunk_size, pointer)
+	# Approximate mag 20 counts values for griz bands
+
+	'''
+	The below code computes the threshold for rejecting galaxies for each field image from the masking algorithm using BC03 models. This isn't necessary for 2DAs
+	I include it here for DR7 Mg II absorbers.
+	'''
+	
+	mag20 = 0
+	if color == 'g':
+		mag20 = 2500
+	if color == 'r':
+		mag20 = 1900
+	if color == 'i':
+		mag20 = 1400
+	if color == 'z':
+		mag20 = 300
+
+	RA = float(linecache.getline('Full Data.txt', i).split()[1])
+	DEC = float(linecache.getline('Full Data.txt', i).split()[2])
+	redshift = 0
+
+	for j in range(len(mgtable)):
+		if abs(mgtable['RA'][j] - RA) < 0.0001 and mgtable['DEC'][j] - DEC < 0.0001 and mgtable['ZABS'][j][0] >= 0.37 and mgtable['ZABS'][j][0] < 0.55:
+			redshift = mgtable['ZABS'][j][0]
+			break
+
+	if redshift == 0:
+		return
+
+
+	thresh = thresh_mags[int(redshift / 0.005)][pointer] + 0.02
+	thresh = mag20 * 10 ** (8 - thresh / 2.5)
+
+    final_image =  mask_algorithms(image, quasar_xc, quasar_yc, obj_table, chunk_size, pointer, thresh)
 
     if final_image.size > 1.0:
         input_shape = star_array.shape
@@ -72,7 +116,7 @@ for i in np.arange(run.size):
 
             if criterion1 and criterion2 and criterion3:
 
-                final_ref =  mask_algorithms(image, quasar_xc, quasar_yc, obj_table, chunk_size, pointer)
+                final_ref =  mask_algorithms(image, quasar_xc, quasar_yc, obj_table, chunk_size, pointer, 0)
 
                 final_ref /= obj_table['PSFFLUX'][j][pointer]
                 final_ref *= obj_table['PSFFLUX'][obj_id - 1][pointer]
@@ -101,7 +145,8 @@ for i in np.arange(run.size):
         final_fit = final_fit.reshape((input_shape))
         mean, median, stddev = sigma_clipped_stats(final_fit)
         final_fit -= median
-
+		
+		
         residue = final_image - final_fit
-        mean, median, stddev = sigma_clipped_stats(residue, sigma=3.0, iters=5)
-        residue -= median
+        #mean, median, stddev = sigma_clipped_stats(residue, sigma=3.0, iters=5)
+        #residue -= median
